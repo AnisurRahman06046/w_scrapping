cbc16df521fac36b62c49a7763ba9798b3c8a3a8
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-02 01:56:30 +0600

    find done

--- part2/t1.py
+++ part2/t1.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://webscraper.io/test-sites/e-commerce/allinone"
+# url = "https://webscraper.io/test-sites/e-commerce/allinone"
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
 r = requests.get(url)
 # soup = BeautifulSoup(r.text,"html.parser")
 soup = BeautifulSoup(r.text,"lxml")
 # print(soup.div.p)
-tag = soup.header 
-# print(tag.attrs['role'])
-li = tag.attrs['class']
-print(li)
-for l in li:
-    print(l)
+# tag = soup.header 
+# # print(tag.attrs['role'])
+# li = tag.attrs['class']
+# print(li)
+# for l in li:
+#     print(l)
+
+price = (soup.find("h4",{"class":"price float-end card-title pull-right"}))
+title = (soup.find("a",{"class":"title"}))
+print(price.string)
+print(title.string)
