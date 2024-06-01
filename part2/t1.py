c83bd68a7dfcebb3cc3c40718e7bf5d3b1a9ad6f
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-02 01:36:12 +0600

    done

---
+++ part2/t1.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://webscraper.io/test-sites/e-commerce/allinone"
+r = requests.get(url)
+# soup = BeautifulSoup(r.text,"html.parser")
+soup = BeautifulSoup(r.text,"lxml")
+print(soup.div)
