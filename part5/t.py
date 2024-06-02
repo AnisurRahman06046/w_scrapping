0107bbd5f493a99407c1ee2fae6071e63a99e9fd
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-03 02:12:21 +0600

    added

--- part5/t.py
+++ part5/t.py
@@ -6,7 +6,13 @@ table = soup.find("table",class_="table table-sm table-hover screenertable")
 table_header = table.find_all("th")
 print(len(table_header))
 headers = [header.text for header in table_header]
-print(headers)
+# table_datas = table.find_all("td")
+# # print(table_datas)
+# datas = [data.text for data in table_datas]
+# print(datas)
 
 # for i in table_header:
 #     print(i.text)
+
+df = pd.DataFrame(columns=headers)
+print(df)
