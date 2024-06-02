66a3a1e6d49cf30488f27e6dffb7e62569a7802a
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-03 01:49:30 +0600

    added

--- part5/t.py
+++ part5/t.py
@@ -1,6 +1,12 @@
 from htmlExtractor import extractHtml
-
 import pandas as pd
 
 soup = extractHtml("https://ticker.finology.in/","html.parser")
-print(soup)
+table = soup.find("table",class_="table table-sm table-hover screenertable")
+table_header = table.find_all("th")
+print(len(table_header))
+headers = [header.text for header in table_header]
+print(headers)
+
+# for i in table_header:
+#     print(i.text)
