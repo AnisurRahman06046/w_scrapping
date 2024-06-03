a73615c2507d6932f2fba1aad3a4f42e426f6d6b
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-03 15:40:21 +0600

    updated

--- part6/p.py
+++ part6/p.py
@@ -11,3 +11,17 @@ headers = [header.text for header in table_headers]
 
 df = pd.DataFrame(columns=headers)
 # print(df)
+
+rows = table.find_all("tr")
+for i in rows[1:]:
+    # # print(i)
+    tr = i.find_all("td")
+    # # print(tr)
+    row = [data.text.strip() for data in tr]
+    l = len(df)
+    df.loc[l]=row
+
+# print(rows)
+print(df)
+
+df.to_csv("table_data.csv")
