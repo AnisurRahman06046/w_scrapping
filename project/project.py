eeedd40baa16cedb62485f40acdd43146b6d172a
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-03 17:51:13 +0600

    done

--- project/project.py
+++ project/project.py
@@ -7,9 +7,22 @@ table = soup.find("table",class_="ih-td-tab auction-tbl",id="t4")
 t_h = table.find_all("th")
 # print(headers)
 headers = [t.text.strip() for t in t_h]
-print(headers)
+# print(headers)
 df = pd.DataFrame(columns=headers)
+# print(df)
+tb = table.find("tbody",id="pointsdata")
+t_r = tb.find_all("tr")
+# print(t_r)
+# datas = [d.text.strip().replace('\n', ' ') for d in t_r]
+for row in t_r:
+    cols = row.find_all("td")
+    # print(cols)
+    data = [col.text.replace('\n', ' ').replace('₹', '').strip() for col in cols]  # Remove newline characters and strip whitespace
+    df.loc[len(df)] = data  # Add the cleaned data to the DataFrame
+
 print(df)
 
+# df.to_csv("ipl_auction.csv")
+df.to_csv("auction.csv")
 
 
